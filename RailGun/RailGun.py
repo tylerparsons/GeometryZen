96bf3111cb6e40eb0ebdd75d52e23b9501ec5465
tylerparsons <tylereparsons13@gmail.com>
2014-05-17 13:40:35 -0400

    Save file.

--- RailGun/RailGun.py
+++ RailGun/RailGun.py
@@ -1,1 +1,60 @@
-# RailGun/RailGun.py
+'''
+RailGun.py
+'''
+
+from browser import *
+from three import *
+from workbench import *
+from geometry import *
+
+
+
+# RailGun Properties
+I = 100
+dt = 0.01
+mu_0 = 12.566E-7
+pi = 3.1415
+w = 1.0     # bore width
+
+# Model Geometry
+space3D = CartesianSpace()
+i = VectorE3(1.0, 0.0, 0.0)
+j = VectorE3(0.0, 1.0, 0.0)
+k = VectorE3(0.0, 0.0, 1.0)
+
+workbench3D = Workbench3D(space3D.renderer.domElement, space3D.renderer, space3D.camera)
+
+# Projectile Definition
+def Projectile(x_0, m):
+    particle = SphereBuilder().color("red").radius(0.1).build()
+    particle.charge   = ScalarE3(1.0)
+    particle.mass     = ScalarE3(m)
+    particle.position = VectorE3(x_0, 0.0, 0.0)
+    particle.velocity = VectorE3(0.0, 0.0, 0.0)
+    space3D.add(particle)
+    return particle
+
+projectile = Projectile(1.0, 1.0)
+
+# Model Animation
+def setup():
+    workbench3D.setUp()
+
+def tick(t):
+    # Define quantities for calculation
+    x = projectile.position
+    m = projectile.mass
+    # Solve equation of motion
+    projectile.acceleration = ((mu_0*I**2*x)/(m*pi))*(sqrt(w**2 + x**2) - x))
+    projectile.velocity = projectile.velocity + projectile.acceleration * dt
+    projectile.position = projectile.position + projectile.velocity * dt
+    
+    space3D.render()
+    
+def terminate(t):
+    pass
+
+def tearDown():
+    workbench3D.tearDown()
+
+WindowAnimationRunner(tick, terminate, setUp, tearDown, window).start()
