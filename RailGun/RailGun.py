8450034ee8235bbe900787ba85b85aebb1d81d3b
tylerparsons <tylereparsons13@gmail.com>
2014-05-17 14:07:44 -0400

    Save file.

--- RailGun/RailGun.py
+++ RailGun/RailGun.py
@@ -9,7 +9,7 @@ from geometry import *
 
 
 # RailGun Properties
-I = 100
+I = 1000
 mu_0 = 12.566E-7
 pi = 3.1415
 w = 1.0     # bore width
@@ -17,10 +17,6 @@ dt = 0.01
 
 # Model Geometry
 space3D = CartesianSpace()
-i = VectorE3(1.0, 0.0, 0.0)
-j = VectorE3(0.0, 1.0, 0.0)
-k = VectorE3(0.0, 0.0, 1.0)
-
 workbench3D = Workbench3D(space3D.renderer.domElement, space3D.renderer, space3D.camera)
 
 # Projectile Definition
@@ -40,13 +36,9 @@ def tick(t):
     x = projectile.position
     m = projectile.mass
     # Solve equation of motion
-    projectile.acceleration = ((mu_0*I**2*x)/(m*pi))*(sqrt(w**2 + x**2) - x)
-    projectile.velocity = projectile.velocity + projectile.acceleration * dt
+    a = ((mu_0*I*I*x)/(m*pi))*(sqrt(w*w + x*x) - x)
+    projectile.velocity = projectile.velocity + a * dt
     projectile.position = projectile.position + projectile.velocity * dt
-    print "tick"
-    print(projectile.acceleration)
-    print(projectile.velocity)
-    print(projectile.position)
     # Follow projectile
     space3D.camera.position.x = projectile.position.x
     space3D.camera.lookAt(projectile.position)
